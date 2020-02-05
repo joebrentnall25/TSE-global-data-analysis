fe3c857461dec115b96b2d84dc81f6bbc31c39f5
netsec917 <netsec917@protonmail.com>
2020-02-05 19:51:36 +0000

    updated docker file for azure deployment

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-COPY requirements.txt /
-RUN pip install -r /requirements.txt
- 
-RUN mkdir /TSE-global-data-analysis
-WORKDIR /TSE-global-data-analysis
-COPY ./ ./
+FROM python:3.7
 
-ENV NAME TSE-GLOBAL-DATA-ANALYSIS
- 
+RUN mkdir /app
+WORKDIR /app
+ADD requirements.txt /app/
+RUN pip install -r requirements.txt
+ADD . /app/
+
 EXPOSE 8050
-CMD ["python", "./index.py"]
+
+ENTRYPOINT [ "python" ]
+CMD ["index.py"]
--- index.py
+++ index.py
@@ -1,13 +1,10 @@
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
 from app import app
 from app import server # Need for heroku procfile
 from layouts import serve_layout
-import uiCallbacks
-import chartCallbacks
+
 
 app.layout = serve_layout
 
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', debug=True)
